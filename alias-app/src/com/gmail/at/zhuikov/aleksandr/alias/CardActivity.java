1c9c6273c2b0e2a872ddf9903498d24d28cba663
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2011-01-10 21:35:40 +0200

    initial project skeleton

---
+++ alias-app/src/com/gmail/at/zhuikov/aleksandr/alias/CardActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.at.zhuikov.aleksandr.alias;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CardActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
